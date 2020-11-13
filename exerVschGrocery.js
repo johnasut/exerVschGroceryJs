172a7a72f818d9a2ab05693954d2b38e7af090ae
John Saunders <john.saunders@pharm.utah.edu>
2020-11-13 12:53:12 -0700

    Grocery JavaScript Assignment

---
+++ exerVschGrocery.js
@@ -0,0 +1,12 @@
+var shopper = {
+    firstName: "Queen",
+    lastName: "Latifa",
+    budget: 125,
+    fullName: function() {
+        return this.firstName + " " + this.lastName;
+    },
+    groceryCart: ["Apples", "Peanuts", "Hammer", "Shovel", "Bleach", "Duct Tape", "Febreeze", "Tarp", "Butter"],
+    isSerialKiller: true
+};
+
+console.log(shopper)
