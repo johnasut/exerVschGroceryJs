2f06bcf0c3e574a36c466fa4b9320a78cbc4e5af
John Saunders <john.saunders@pharm.utah.edu>
2020-11-13 23:43:28 +0000

    Additional Function work

--- exerVschGrocery.js
+++ exerVschGrocery.js
@@ -1,13 +1,40 @@
 var shopper = {
     firstName: "Queen",
     lastName: "Latifa",
-    budget: 125,
+    budget: 725,
     fullName: function() {
         return this.firstName + " " + this.lastName;
     },
-    groceryCart: ["Apples", "Peanuts", "Hammer", "Shovel", "Bleach", "Duct Tape", "Febreeze", "Tarp", "Butter"],
-    isSerialKiller: true
+    groceryCart: [],
+    isSerialKiller: false,
+    addItem: function(item, cost) {
+      if (cost < this.budget && (item === "hammer" || item === "shovel" || item === "tarp" || item === "duct tape" || item === "bleach" || item === "rope")) {
+        this.budget -= cost;
+        this.groceryCart.push(item);
+        this.isSerialKiller = true;
+        console.log("Added " + item + " to cart. " + this.budget + " remaining. Watch out for this guy.");
+      }
+      else if (cost < this.budget) {
+        this.budget -= cost;
+        this.groceryCart.push(item);
+        console.log("Added " + item + " to cart. " + this.budget + " remaining.");
+      }
+      else {  
+        console.log("Insufficient funds for " + item + ".");
+      }
+    }
 };
 
-console.log(shopper.groceryCart)
-console.log(shopper.budget);
+shopper.addItem("apple", 2);
+shopper.addItem("peanuts", 5);
+shopper.addItem("butter", 6);
+console.log(shopper.isSerialKiller);
+shopper.addItem("hammer", 50);
+shopper.addItem("shovel", 50);
+shopper.addItem("tarp", 15);
+shopper.addItem("duct tape", 10);
+shopper.addItem("rope", 80);
+console.log(shopper.isSerialKiller);
+shopper.addItem("Febreeze", 50);
+shopper.addItem("handbag", 3000)
+console.log(shopper.isSerialKiller);
